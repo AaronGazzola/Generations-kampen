0bea45125796c496cdcda6c41f99d016f7e23890
Aaron Gazzola <aaron@apexapps.com.au>
2021-09-30 14:49:48 +1000

    liquid animation

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,124 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Keyframes } from '../components/Keyframes';
 import Meta from '../components/Meta';
+import { useAppSelector } from '../redux/hooks';
 
 const Home: NextPage = () => {
-	const [phase, setPhase] = useState<'standby' | 'play' | 'feedback'>(
-		'standby'
-	);
+	const { screenWidth, screenHeight } = useAppSelector(state => state.utils);
+	const [playButtonStyle, setPlayButtonStyle] = useState({
+		height: 100,
+		width: 300,
+		color: 'green'
+	});
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+	const [phase, setPhase] = useState<
+		'standby' | 'play' | 'feedback' | 'countdown'
+	>('standby');
 	const [seconds, setSeconds] = useState(0);
 
+	useEffect(() => {
+		if (phase !== 'play' || !canvasRef.current) return;
+		canvasCtxRef.current = canvasRef.current.getContext('2d');
+		let aniId;
+		canvasRef.current.width = screenWidth;
+		canvasRef.current.height = screenHeight + 100;
+		let w = screenWidth,
+			h = screenHeight,
+			particles: { x: number; y: number; d: number; respawn: () => void }[] =
+				[],
+			fill = true,
+			color = '#63BD61',
+			c: number,
+			level = 100;
+
+		interface Particle {
+			x: number;
+			y: number;
+			d: number;
+			respawn?: () => void;
+		}
+
+		function particle(this: Particle, x: number, y: number, d: number) {
+			this.x = x;
+			this.y = y;
+			this.d = d;
+			this.respawn = function () {
+				this.x = Math.random() * (w * 0.8) + 0.1 * w;
+				this.y = Math.random() * 30 + h - ((h - 100) * level) / 100 - 50 + 50;
+				this.d = Math.random() * 5 + 5;
+			};
+		}
+
+		const init = () => {
+			c = 0;
+			particles = [];
+			particles = [];
+			for (var i = 0; i < 40; i++) {
+				var obj = new (particle as any)(0, 0, 0);
+				obj.respawn();
+				particles.push(obj);
+			}
+			aniId = window.requestAnimationFrame(draw);
+		};
+
+		const draw = () => {
+			if (!canvasCtxRef.current) return;
+			canvasCtxRef.current.clearRect(0, 0, w, h);
+			canvasCtxRef.current.fillStyle = color;
+			canvasCtxRef.current.strokeStyle = color;
+
+			//draw the liquid
+			canvasCtxRef.current.beginPath();
+			canvasCtxRef.current.moveTo(w, h - ((h - 100) * level) / 100 - 50);
+			canvasCtxRef.current.lineTo(w, h);
+			canvasCtxRef.current.lineTo(0, h);
+			canvasCtxRef.current.lineTo(0, h - ((h - 100) * level) / 100 - 50);
+			var temp = 50 * Math.sin((c * 1) / 50);
+			canvasCtxRef.current.bezierCurveTo(
+				w / 3,
+				h - ((h - 100) * level) / 100 - 50 - temp,
+				(2 * w) / 3,
+				h - ((h - 100) * level) / 100 - 50 + temp,
+				w,
+				h - ((h - 100) * level) / 100 - 50
+			);
+			canvasCtxRef.current.fill();
+
+			//draw the bubbles
+			for (var i = 0; i < 40; i++) {
+				canvasCtxRef.current.beginPath();
+				canvasCtxRef.current.arc(
+					particles[i].x,
+					particles[i].y,
+					particles[i].d,
+					0,
+					2 * Math.PI
+				);
+				if (fill) canvasCtxRef.current.fill();
+				else canvasCtxRef.current.stroke();
+			}
+
+			update();
+			aniId = window.requestAnimationFrame(draw);
+		};
+
+		const update = () => {
+			c++;
+			if (100 * Math.PI <= c) c = 0;
+			for (var i = 0; i < 40; i++) {
+				particles[i].x = particles[i].x + Math.random() * 2 - 1;
+				particles[i].y = particles[i].y - 1;
+				particles[i].d = particles[i].d - 0.04;
+				if (particles[i].d <= 0) particles[i].respawn();
+			}
+		};
+		if (seconds === 60) init();
+	}, [seconds, phase]);
+
 	const startTriviaTimer = () => {
+		setPhase('play');
 		let sec = 60;
 		setSeconds(sec);
 		const timer = setInterval(() => {
@@ -23,14 +133,14 @@ const Home: NextPage = () => {
 	};
 
 	const playHandler = () => {
-		let sec = 3;
+		setPhase('countdown');
+		let sec = 4;
 		setSeconds(sec);
 		const timer = setInterval(() => {
 			sec--;
 			setSeconds(sec);
 			if (sec === 0) {
 				clearInterval(timer);
-				setPhase('play');
 				startTriviaTimer();
 			}
 		}, 1000);
@@ -39,13 +149,91 @@ const Home: NextPage = () => {
 	return (
 		<>
 			<Meta />
-			{phase === 'standby' ? (
-				<button
-					className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-green-500 text-white font-semibold w-20 h-10'
-					onClick={playHandler}
-				>
-					{seconds === 0 ? 'Play' : seconds}
-				</button>
+			<Keyframes
+				from={{
+					transform: `scale(${playButtonStyle.width / screenWidth}, ${
+						playButtonStyle.height / screenHeight
+					})`,
+					borderRadius: 100
+				}}
+				to={{ transform: 'scale(1.2,1.2)', borderRadius: 9999999 }}
+				name='playButtonExpand'
+			/>
+			{/* Play button */}
+			<div
+				className={`fixed top-0 left-0 w-screen origin-center bg-${
+					playButtonStyle.color
+				} ${
+					['standby', 'countdown'].includes(phase) ? '' : 'hidden'
+				} cursor-pointer`}
+				style={{
+					height: 'calc(var(--vh) * 100)',
+					transform: `scale(${playButtonStyle.width / screenWidth}, ${
+						playButtonStyle.height / screenHeight
+					})`,
+					borderRadius: 100,
+					animation:
+						phase === 'countdown'
+							? 'playButtonExpand .5s cubic-bezier( 0.87, -0.1, 0.98, 0.06 )  forwards'
+							: ''
+				}}
+			></div>
+			<button
+				className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-semibold text-4xl ${
+					['standby', 'countdown'].includes(phase) ? '' : 'hidden'
+				}`}
+				onMouseOver={() =>
+					setPlayButtonStyle(prev => ({ ...prev, color: 'green-light' }))
+				}
+				onMouseOut={() =>
+					setPlayButtonStyle(prev => ({ ...prev, color: 'green' }))
+				}
+				onMouseUp={playHandler}
+				style={{
+					width: playButtonStyle.width,
+					height: playButtonStyle.height,
+					transform:
+						phase === 'countdown' ? `scale(6) translate(-8%, -8%)` : '',
+					opacity: phase === 'countdown' ? 0 : 1,
+					transition: 'all .5s cubic-bezier( 0.87, -0.1, 0.98, 0.06 )'
+				}}
+				onClick={playHandler}
+			>
+				Play
+			</button>
+			{/* Countdown */}
+			<p
+				className={`fixed top-1/2 left-1/2 text-7xl text-white ${
+					phase === 'countdown' && seconds !== 4 ? '' : 'hidden'
+				}`}
+				style={{
+					animation:
+						phase === 'countdown' && [1, 2, 3].includes(seconds)
+							? 'countdown 1s ease infinite'
+							: ''
+				}}
+			>
+				{seconds}
+			</p>
+
+			{/* Liquid timer */}
+			<canvas
+				className={`fixed top-0 left-0 ${phase === 'play' ? '' : 'hidden'}`}
+				ref={canvasRef}
+				style={{
+					animation: 'drain 60s linear forwards'
+				}}
+			></canvas>
+			<div
+				className={`w-screen h-screen bg-green fixed top-full left-0 transform -translate-y-14 ${
+					phase === 'play' && seconds > 50 ? '' : 'hidden'
+				}`}
+			></div>
+
+			{/* <div className={`fixed top-0 left-0 w-screen ${phase === 'play' ? '' : 'hidden'}`} style={{height:'calc((var(--vh) * 100)'}}></div> */}
+
+			{/* {phase === 'standby' ? (
+				<></>
 			) : (
 				<div className='absolute top-0 left-0 w-full h-screen flex flex-col justify-between p-2'>
 					<div>
@@ -90,7 +278,7 @@ const Home: NextPage = () => {
 						))}
 					</div>
 				</div>
-			)}
+			)} */}
 		</>
 	);
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				green: {
+					dark: '#1b8433',
+					DEFAULT: '#63BD61',
+					light: '#72c371'
+				},
+				yellow: {
+					DEFAULT: '#ffc107'
+				},
+				red: {
+					DEFAULT: '#dc3545'
+				}
+			}
+		}
 	},
 	variants: {
 		extend: {}
