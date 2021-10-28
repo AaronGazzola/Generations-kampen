77d8fa57aae37e4b6ca531d64901ed2bf7833467
Aaron Gazzola <aaron@apexapps.com.au>
2021-10-28 15:22:39 +1100

    wait for media to load before allowing gameplay

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -40,6 +40,7 @@ const Home: NextPage = () => {
 	);
 	const [bubbles, setBubbles] = useState<Bubble[]>([]);
 	const [gotTrivia, setGotTrivia] = useState<boolean>(false);
+	const [mediaIsReady, setMediaIsReady] = useState<boolean>(false);
 	const [showScrollIcon, setShowScrollIcon] = useState<boolean>(true);
 	const [videoWidth, setVideoWidth] = useState<string>('320px');
 	const [selectedAnswer, setSelectedAnswer] = useState<string>('');
@@ -63,6 +64,7 @@ const Home: NextPage = () => {
 	};
 
 	const playHandler = () => {
+		if (!mediaIsReady) return;
 		setPhase('countdown');
 		setSeconds(5);
 		setBubbles(getBubbles(30));
@@ -183,6 +185,25 @@ const Home: NextPage = () => {
 	useEffect(() => {
 		if (phase !== 'standby') noSleepVideoRef.current?.pause();
 	}, [phase]);
+
+	useEffect(() => {
+		let timer: NodeJS.Timer;
+		if (phase === 'standby') {
+			timer = setInterval(() => {
+				if (
+					videoRef.current?.readyState === 4 &&
+					countdownVideoRef.current?.readyState === 4
+				)
+					setMediaIsReady(true);
+				if (mediaIsReady) {
+					clearInterval(timer);
+				}
+			}, 500);
+		} else {
+			setMediaIsReady(false);
+		}
+		return () => clearInterval(timer);
+	}, [phase, mediaIsReady]);
 
 	return (
 		<>
@@ -341,7 +362,22 @@ const Home: NextPage = () => {
 									fontFamily: "'Londrina Solid', sans-serif"
 								}}
 							>
-								STARTA
+								{mediaIsReady ? (
+									'STARTA'
+								) : (
+									<svg
+										className='fill-current text-yellow-dark w-10 h-12'
+										style={{
+											animation: '2s loading linear infinite'
+										}}
+										xmlns='http://www.w3.org/2000/svg'
+										width='24'
+										height='24'
+										viewBox='0 0 24 24'
+									>
+										<path d='M13.75 22c0 .966-.783 1.75-1.75 1.75s-1.75-.784-1.75-1.75.783-1.75 1.75-1.75 1.75.784 1.75 1.75zm-1.75-22c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm10 10.75c.689 0 1.249.561 1.249 1.25 0 .69-.56 1.25-1.249 1.25-.69 0-1.249-.559-1.249-1.25 0-.689.559-1.25 1.249-1.25zm-22 1.25c0 1.105.896 2 2 2s2-.895 2-2c0-1.104-.896-2-2-2s-2 .896-2 2zm19-8c.551 0 1 .449 1 1 0 .553-.449 1.002-1 1-.551 0-1-.447-1-.998 0-.553.449-1.002 1-1.002zm0 13.5c.828 0 1.5.672 1.5 1.5s-.672 1.501-1.502 1.5c-.826 0-1.498-.671-1.498-1.499 0-.829.672-1.501 1.5-1.501zm-14-14.5c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm0 14c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2z' />
+									</svg>
+								)}
 							</button>
 							<div
 								className='relative'
