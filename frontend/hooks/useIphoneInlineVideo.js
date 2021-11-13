9d5484ff3d3dd87348d4c6e66b965f7a62e6067b
Aaron Gazzola <aaron@apexapps.com.au>
2021-11-13 20:12:51 +1100

    minor fix to iphoneinlinevideo hook

--- frontend/hooks/useIphoneInlineVideo.js
+++ frontend/hooks/useIphoneInlineVideo.js
@@ -14,7 +14,6 @@ const useIphoneInlineVideo = () => {
 			enableInlineVideo(questionVideo);
 			enableInlineVideo(countdownVideo);
 			setIsInline(true);
-			console.log('done');
 			screenActiveVideo.play();
 		}
 	}, [mounted]);
